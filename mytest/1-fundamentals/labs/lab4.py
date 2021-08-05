a34451981ba338b1af605baf8c1527eae56a5fd0
dontran10 <64119388+dontran10@users.noreply.github.com>
2021-08-05 23:18:25 +0700

    ok

--- mytest/1-fundamentals/labs/lab4.py
+++ mytest/1-fundamentals/labs/lab4.py
@@ -1,11 +1,23 @@
 def find_student_average_mark(students, query_name):
     result = None
     # TODO: Enjoy your solving here
+    student = next((x for x in students if x[0] == query_name), [None])
+    if student != None:
+        return "{:.2f}".format((float(student[1]) + float(student[2]) + float(student[3]))/3)
     return result
 
 
 if __name__ == '__main__':
-    students = None
+    students = []
     query_name = None
     # TODO: Collect input information from console
-    print(find_student_average_mark(students, query_name))
+    N = int(input("Enter numer of students:"))
+    if (N <= 2) or (N > 10):
+        print('Invalid number 2 < N <= 10')
+    else:
+        for i in range(N):
+            strtext = str(input("Student " + str(i) + ": "))
+            student = strtext.split(' ')
+            students.append(student)
+        query_name = str(input("Query name:"))
+        print(find_student_average_mark(students, query_name))
