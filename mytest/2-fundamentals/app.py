726914cbf911b6473ff4492603b6734c1c939e71
dontran10 <64119388+dontran10@users.noreply.github.com>
2021-08-07 00:05:20 +0700

    k

--- mytest/2-fundamentals/app.py
+++ mytest/2-fundamentals/app.py
@@ -1,5 +1,16 @@
 import module11
+import common_funcs
+import packing_classes
 
 if __name__ == '__main__':
-    y = str(input("Enter a number:"))
-    print(module11.checkDigitCalculation(y))
+    option = None
+    while option not in {1, 2, 3}:
+        option = int(input("Select Option Number [1-Park, 2-Pickup, 3-History]:"))
+    
+    packing = packing_classes.Packing()
+    if option == 1:
+        packing.pack()
+    elif option == 2:
+        packing.pickup()
+    else:
+        packing.history()
